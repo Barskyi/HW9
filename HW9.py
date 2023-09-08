4787ebca189e53c3aa67a36a372528ea9f3e3f34
Barskyi <137706923+Barskyi@users.noreply.github.com>
2023-09-08 19:32:07 +0300

    Update HW9.py

--- HW9.py
+++ HW9.py
@@ -4,40 +4,44 @@ import click
 contacts = {}
 
 
-# Функція для обробки команди "hello"
-def hello():
-    click.echo("How can I help you?")
+# Декоратор input_error для обробки помилок введення користувача
+def input_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            click.echo("Contact not found.")
+        except ValueError:
+            click.echo("Invalid input. Please enter name and phone separated by space.")
+        except IndexError:
+            click.echo("Invalid input format.")
+    return wrapper
 
 
 # Функція для обробки команди "add"
+@input_error
 def add(contact_info):
-    try:
-        name, phone = contact_info.split()
-        contacts[name] = phone
-        click.echo(f"Contact '{name}' added successfully.")
-    except ValueError:
-        click.echo("Invalid input. Please enter name and phone separated by space.")
+    name, phone = contact_info.split()
+    contacts[name] = phone
+    click.echo(f"Contact '{name}' added successfully.")
 
 
 # Функція для обробки команди "change"
+@input_error
 def change(contact_info):
-    try:
-        name, phone = contact_info.split()
-        if name in contacts:
-            contacts[name] = phone
-            click.echo(f"Phone number for '{name}' updated successfully.")
-        else:
-            click.echo(f"Contact '{name}' not found.")
-    except ValueError:
-        click.echo("Invalid input. Please enter name and phone separated by space.")
+    name, phone = contact_info.split()
+    if name in contacts:
+        contacts[name] = phone
+        click.echo(f"Phone number for '{name}' updated successfully.")
+    else:
+        raise KeyError
 
 
 # Функція для обробки команди "phone"
+@input_error
 def phone(contact_name):
-    if contact_name in contacts:
-        click.echo(f"Phone number for '{contact_name}': {contacts[contact_name]}")
-    else:
-        click.echo(f"Contact '{contact_name}' not found.")
+    phone_number = contacts[contact_name]
+    click.echo(f"Phone number for '{contact_name}': {phone_number}")
 
 
 # Функція для обробки команди "show all"
@@ -57,7 +61,7 @@ def main():
     while True:
         user_input = input("Enter a command: ").lower()
         if user_input == "hello":
-            hello()
+            click.echo("How can I help you?")
         elif user_input.startswith("add "):
             add(user_input[4:])
         elif user_input.startswith("change "):
